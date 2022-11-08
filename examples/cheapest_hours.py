8c71294d47deba82064496b28a3a7164990e3906
Matti Lattu <matti.lattu@iki.fi>
2022-11-08 17:38:59 +0200

    Renamed object variable name to charify difference between class and object

--- examples/cheapest_hours.py
+++ examples/cheapest_hours.py
@@ -17,12 +17,12 @@ prices_spot = elspot.Prices()
 if os.path.isfile(SQLITE_DB):
     print(f'Warning: Using your existing database at {SQLITE_DB}')
 
-nordpool_db = nordpool_db.NordpoolDb(SQLITE_DB)
+npdb = nordpool_db.NordpoolDb(SQLITE_DB)
 tz = pytz.timezone(TZ)
 dt_yesterday = tz.localize(datetime.today()-timedelta(days=1))
 
 # Fetch yesterday
-nordpool_db.update_data(prices_spot.hourly(areas=[AREA], end_date=dt_yesterday))
+npdb.update_data(prices_spot.hourly(areas=[AREA], end_date=dt_yesterday))
 
 dt_yesterday_start = datetime.strptime(dt_yesterday.strftime('%Y-%m-%d 00:00:00'), '%Y-%m-%d %H:%M:%S')
 dt_yesterday_end = dt_yesterday_start + timedelta(days=1)
@@ -32,8 +32,8 @@ time_pointer = dt_yesterday_start
 while (time_pointer < dt_yesterday_end):
     print(time_pointer.strftime('%Y-%m-%d %H:%M'), end='\t')
 
-    price = nordpool_db.get_price_value(AREA, time_pointer)
-    rank = nordpool_db.get_price_rank(AREA, dt_yesterday_start, dt_yesterday_end, time_pointer)
+    price = npdb.get_price_value(AREA, time_pointer)
+    rank = npdb.get_price_rank(AREA, dt_yesterday_start, dt_yesterday_end, time_pointer)
 
     print(f'{price}\t{rank[0]}')
 
--- examples/dump_prices.py
+++ examples/dump_prices.py
@@ -14,20 +14,20 @@ prices_spot = elspot.Prices()
 if os.path.isfile(SQLITE_DB):
     print(f'Warning: Using your existing database at {SQLITE_DB}')
 
-nordpool_db = nordpool_db.NordpoolDb(SQLITE_DB)
+npdb = nordpool_db.NordpoolDb(SQLITE_DB)
 
 dt_today = datetime.today()
 dt_yesterday = datetime.today()-timedelta(days=1)
 dt_tomorrow = datetime.today()+timedelta(days=1)
 
 # Fetch today
-nordpool_db.update_data(prices_spot.hourly(areas=AREAS, end_date=dt_today))
+npdb.update_data(prices_spot.hourly(areas=AREAS, end_date=dt_today))
 
 # Fetch yesterday
-nordpool_db.update_data(prices_spot.hourly(areas=AREAS, end_date=dt_yesterday))
+npdb.update_data(prices_spot.hourly(areas=AREAS, end_date=dt_yesterday))
 
 # Fetch tomorrow
-nordpool_db.update_data(prices_spot.hourly(areas=AREAS, end_date=dt_tomorrow))
+npdb.update_data(prices_spot.hourly(areas=AREAS, end_date=dt_tomorrow))
 
 # Create start/pointer time and end time for a loop
 time_current = datetime.strptime(dt_yesterday.strftime('%Y-%m-%d 00:00:00'), '%Y-%m-%d %H:%M:%S')
@@ -37,7 +37,7 @@ while (time_current <= time_end):
     print(time_current.strftime('%Y-%m-%d %H:%M'), end='\t')
 
     for this_area in AREAS:
-        price = nordpool_db.get_price_value(this_area, time_current)
+        price = npdb.get_price_value(this_area, time_current)
         print(f'{this_area}:{price}', end='\t')
 
     time_current += timedelta(hours=1)
